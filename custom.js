7d7c738fc2a1d0641783e2452cef53048177a131
Emanuel Mestrovic <mestro67@hotmail.com>
2023-02-10 23:08:00 +0100

    Sync with live version

--- custom.js
+++ custom.js
@@ -1,3 +1,21 @@
-$(function(){
-	
+$(document).ready(function() {
+    $('#form').submit(function(event) {
+        event.preventDefault();
+
+        $.ajax({
+            type: 'POST',
+            url: 'sentMail.php',
+            data: $('#form').serialize(),
+            success: function(response) {
+                if (response == 'success') {
+                    // ('#message').html('Form submitted successfully!');
+                } else {
+                    // $('#message').html('Error: Form submission failed.');
+                }
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                //$('#message').html('Error: Form submission failed.');
+            }
+        });
+    });
 });
--- sentMail.php
+++ sentMail.php
@@ -1,21 +1,22 @@
 <?php
-
-if(isset($_POST['submit'])) {
-    $mailto = "pilana.ivandol@gmail.com"; //E-mail adresa Pilane Ivandol
+    // $mailto = "pilana.ivandol@gmail.com"; //E-mail adresa Pilane Ivandol
+    $mailto = "mestro67@hotmail.com"; //E-mail adresa Pilane Ivandol
 
     //korisnikovi podaci
 
     $name = $_POST['name']; //ime korisnika
     $fromEmail = $_POST['email']; //e-mail korisnika
     $phone = $_POST['phone']; //telefon
-    $subject = $_POST['subject']; //naslov poruke
-    $subject2 = "Vaša poruka je uspješno poslana." 
+    $subject = "Upit s web stranice: Pilana Ivandol"; // $_POST['subject']; //naslov poruke
+    $subject2 = "Vaša poruka je uspješno poslana.";
+    $message = "";
+    $message2 = "";
 
     //Primljena poruka pilani
 
-    $message = "Cleint Name: " . $name . "\n"
-    . "Phone Number: " . $phone . "\n\n"
-    . "Client Message: " . "\n" . $_POST['message']
+    $message = "Ime: " . $name . "\n"
+    . "Telefonski broj: " . $phone . "\n\n"
+    . "Poruka: " . "\n" . $_POST['message'];
 
     //Poruka za potvrdu korisniku
     $message2 = "Poštovani" . $name . "\n"
@@ -24,20 +25,20 @@ if(isset($_POST['submit'])) {
     . "Lijep pozdrav, " . "\n" . "Pilana Ivandol";
 
     //Email header
-    $headers = "From: " . $fromEmail; //primljeni email korisnika
-    $headers2 = "From: " . $mailto; //email kojem će korisnik biti primatelj
+    $headers = "Od: " . $fromEmail; //primljeni email korisnika
+    $headers2 = "Od: " . $mailto; //email kojem će korisnik biti primatelj
 
     //PHP mailer function
      $result1 = mail($mailto, $subject, $message, $headers); //email poslan Pilani Ivandol
-     $result2 = mail($fromEmail, $subject2, $message2, $headers2); //email klijentu
+     // $result2 = mail($fromEmail, $subject2, $message2, $headers2); //email klijentu
 
      //Provjera uspješnog slanja emaila
 
-    if ($result1 && $result2) {
+    if ($result1) {
         $success = "Vaša poruka je uspješno poslana.";
+        echo 'success';
     } else {
         $failed = "Poruka nije poslana. Pokušajte ponovno!";
+        echo 'error';
     }
-}
-
 ?>
